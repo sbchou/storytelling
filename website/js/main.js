cbddb2c28124eec28b0c325b32e26d1905cee0b3
Ruriko Araki <ruriko.araki@gmail.com>
2014-04-26 14:47:17 -0400

    Moved all JS to main,js

--- website/js/main.js
+++ website/js/main.js
@@ -1,1 +1,53 @@
 
+var w = 960,
+h = 500,
+fill = d3.scale.category20();
+
+var vis = d3.select("#chart").append("svg:svg")
+.attr("width", w)
+.attr("height", h);
+
+var force = d3.layout.force()
+.charge(-120)
+.linkDistance(80)
+.size([w, h]);
+
+$(dropdown).
+
+d3.json("test.json", function(json) {
+	force
+	.nodes(json.nodes)
+	.links(json.links)
+	.start();
+
+	var link = vis.selectAll("line.link")
+	.data(json.links)
+	.enter().append("svg:line")
+	.attr("class", "link")
+	.style("stroke-width", function(d) { return Math.sqrt(d.value); })
+	.attr("x1", function(d) { return d.source.x; })
+	.attr("y1", function(d) { return d.source.y; })
+	.attr("x2", function(d) { return d.target.x; })
+	.attr("y2", function(d) { return d.target.y; });
+
+	var node = vis.selectAll("text.node")
+	.data(json.nodes) 
+	.enter().append("svg:text")
+	.attr("class", "node")
+	.attr("x", function(d) { return d.x; })
+	.attr("y", function(d) { return d.y; })
+	.text(function(d) { return d.name; })
+	.style("fill", "black")
+	.style("font", function(d) { return Math.sqrt(d.size / 2) + "px sans-serif"; })
+	.call(force.drag);
+
+	force.on("tick", function() {
+		link.attr("x1", function(d) { return d.source.x; })
+		.attr("y1", function(d) { return d.source.y; })
+		.attr("x2", function(d) { return d.target.x; })
+		.attr("y2", function(d) { return d.target.y; });
+
+		node.attr("x", function(d) { return d.x; })
+		.attr("y", function(d) { return d.y; });
+	});
+});
