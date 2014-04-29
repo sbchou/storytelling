4e1db1dabe9018c970ffc37960eed31f64b09a16
Ruriko Araki <ruriko.araki@gmail.com>
2014-04-28 22:40:02 -0400

    Integrated CSV support into JS

--- website/js/main.js
+++ website/js/main.js
@@ -9,48 +9,70 @@ var vis = d3.select("#chart").append("svg:svg")
 
 var force = d3.layout.force()
 .charge(-120)
-.linkDistance(80)
+.linkDistance(250)
 .size([w, h]);
 
+var data = {};
+
 $('.dropdown-menu li a').click(function () {
-	alert($(this).text());
+	d3.csv("keyword_counts.csv", 
+		function(d) {
+			return {
+				name: d.NAME,
+				size: +d.SIZE
+			}
+		},
+		function(counts) {
+			data.nodes = counts;
+			console.log(data);
 
-	d3.json("test.json", function(json) {
-	force
-	.nodes(json.nodes)
-	.links(json.links)
-	.start();
+			d3.csv("keyword_connections.csv",
+				function(d) {
+					return {
+						source: +d.SOURCE,
+						target: +d.TARGET,
+						value: +d.VALUE
+					}
+				},
+				function(connections) {
+					data.links = connections;
 
-	var link = vis.selectAll("line.link")
-	.data(json.links)
-	.enter().append("svg:line")
-	.attr("class", "link")
-	.style("stroke-width", function(d) { return Math.sqrt(d.value); })
-	.attr("x1", function(d) { return d.source.x; })
-	.attr("y1", function(d) { return d.source.y; })
-	.attr("x2", function(d) { return d.target.x; })
-	.attr("y2", function(d) { return d.target.y; });
+					force
+					.nodes(data.nodes)
+					.links(data.links)
+					.start();
 
-	var node = vis.selectAll("text.node")
-	.data(json.nodes) 
-	.enter().append("svg:text")
-	.attr("class", "node")
-	.attr("x", function(d) { return d.x; })
-	.attr("y", function(d) { return d.y; })
-	.text(function(d) { return d.name; })
-	.style("fill", "black")
-	.style("font", function(d) { return Math.sqrt(d.size / 2) + "px sans-serif"; })
-	.call(force.drag);
+					var link = vis.selectAll("line.link")
+					.data(data.links)
+					.enter().append("svg:line")
+					.attr("class", "link")
+					.style("stroke-width", function(d) { return Math.sqrt(d.value / 2); })
+					.attr("x1", function(d) { return d.source.x; })
+					.attr("y1", function(d) { return d.source.y; })
+					.attr("x2", function(d) { return d.target.x; })
+					.attr("y2", function(d) { return d.target.y; });
 
-	force.on("tick", function() {
-		link.attr("x1", function(d) { return d.source.x; })
-		.attr("y1", function(d) { return d.source.y; })
-		.attr("x2", function(d) { return d.target.x; })
-		.attr("y2", function(d) { return d.target.y; });
+					var node = vis.selectAll("text.node")
+					.data(data.nodes) 
+					.enter().append("svg:text")
+					.attr("class", "node")
+					.attr("x", function(d) { return d.x; })
+					.attr("y", function(d) { return d.y; })
+					.text(function(d) { return d.name; })
+					.style("fill", "black")
+					.style("font", function(d) { return Math.sqrt(d.size) * 1.5 + "px sans-serif"; })
+					.call(force.drag);
 
-		node.attr("x", function(d) { return d.x; })
-		.attr("y", function(d) { return d.y; });
+					force.on("tick", function() {
+						link.attr("x1", function(d) { return d.source.x; })
+						.attr("y1", function(d) { return d.source.y; })
+						.attr("x2", function(d) { return d.target.x; })
+						.attr("y2", function(d) { return d.target.y; });
+
+						node.attr("x", function(d) { return d.x; })
+						.attr("y", function(d) { return d.y; });
+				});
+		});
 	});
-});
 });
 
